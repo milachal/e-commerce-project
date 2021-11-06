0b8217eb280822ea9040d4284b99655d37e340fe
milachal <mila.chalashkanova@ft.com>
2021-11-06 16:07:06 +0200

    Change axios base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,7 @@
 import axios from 'axios';
 
 const authAPI = axios.create({
-  baseURL: process.env.REACT_APP_ENV === 'prod' ?
-    process.env.REACT_APP_PROD_API : process.env.REACT_APP_DEV_API,
+  baseURL: process.env.REACT_APP_PROD_API,
   withCredentials: true,
 });
 
